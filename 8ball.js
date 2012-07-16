0b4f2e285cfeb8e9e06f7e8c7c069bc042ef8dab
semantiks <semantiks@debian.(none)>
2012-07-16 10:40:13 -0400

    adding 8ball plugin

---
+++ 8ball.js
@@ -0,0 +1,46 @@
+/**
+ * @module 8ball
+ */
+
+const irc   = require( "irc-js" )
+    , fmt   = require( "util" ).format
+
+const responses =
+  [ 'It is certain'
+  , 'It is decidedly so'
+  , 'Without a doubt'
+  , 'Yes – definitely'
+  , 'You may rely on it'
+  , 'As I see it, yes'
+  , 'Most likely'
+  , 'Outlook good'
+  , 'Yes'
+  , 'Signs point to yes'
+  , 'Reply hazy, try again'
+  , 'Ask again later'
+  , 'Better not tell you now'
+  , 'Cannot predict now'
+  , 'Concentrate and ask again'
+  , 'Don\'t count on it'
+  , 'My reply is no'
+  , 'My sources say no'
+  , 'Outlook not so good'
+  , 'Very doubtful'
+  ]
+
+const getFortune = function( msg ) {
+  msg.reply( responses[ Math.round( ( responses.length - 1 ) * Math.random() ) ] )
+}
+
+const load = function( client ) {
+  client.lookFor( fmt( "^:%s\\b(.+)\\?\s*$", client.user.nick ), getFortune )
+  return irc.STATUS.SUCCESS
+}
+
+const eject = function() {
+  return irc.STATUS.SUCCESS
+}
+
+exports.name  = "8ball"
+exports.load  = load
+exports.eject = eject
